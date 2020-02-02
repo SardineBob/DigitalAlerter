17076657cbec62f1bef53fc02981fa32301b3d0e
SardineBob <yushlai1987@gmail.com>
2020-02-02 15:04:17 +0800

    完成保全器材標籤基礎物件

--- component/MainWindow.py
+++ component/MainWindow.py
@@ -1,24 +1,30 @@
 import tkinter as tk
 from component.Map import Map
+from component.Tag import Tag
 
 
 class MainWindow():
-    __mainWindowWidth = 640
-    __mainWindowHeight = 480
+    __originWidth = 640
+    __originHeight = 480
+    __curWidth = __originWidth
+    __curHeight = __originHeight
     __mainWindow = None
     __map = None
+    __tag = None
 
     def __init__(self):
         # 準備主要視窗設定
         self.__mainWindow = tk.Tk()
         self.__mainWindow.title("Digital Alerter(ver.0.0.1)")
         self.__mainWindow.geometry("%dx%d" % (
-            self.__mainWindowWidth, self.__mainWindowHeight))
+            self.__originWidth, self.__originHeight))
         # 註冊視窗事件
         self.__mainWindow.bind('<Configure>', self.__windowResize)
         # 產生地圖物件
         self.__map = Map()
-        self.__map.Create(self.__mainWindowWidth, self.__mainWindowHeight)
+        self.__map.Create(self.__originWidth, self.__originHeight)
+        # 產生保全器材標籤位置
+        self.__tag = Tag()
         # 開啟視窗
         self.__mainWindow.mainloop()
 
@@ -26,9 +32,18 @@ class MainWindow():
         # 判斷事件是主要視窗所觸發
         if str(event.widget) == '.':
             # 判斷變動主要視窗寬高的操作
-            if self.__mainWindowWidth != event.width or self.__mainWindowHeight != event.height:
+            if self.__curWidth != event.width or self.__curHeight != event.height:
                 # 觸發執行Map Resize動作
                 self.__map.Create(event.width, event.height)
+                # 重新定位保全器材標籤位置
+                self.__tag.Relocate({
+                    'curWindowWidth': event.width,
+                    'curWindowHeight': event.height,
+                    'oriWindowWidth': self.__originWidth,
+                    'oriWindowHeight': self.__originHeight,
+                    'oriMapWidth': self.__map.mapOriginWidth,
+                    'oriMapHeight': self.__map.mapOriginHeight
+                })
                 # 更新目前視窗寬高
-                self.__mainWindowWidth = event.width
-                self.__mainWindowHeight = event.height
+                self.__curWidth = event.width
+                self.__curHeight = event.height
--- component/Map.py
+++ component/Map.py
@@ -3,24 +3,24 @@ from PIL import Image, ImageTk
 
 
 class Map():
+    mapOriginWidth = 0
+    mapOriginHeight = 0
     __mapPath = './resource/map.jpg'
     __mapLoad = None
-    __mapWidth = None
-    __mapHeight = None
     __mapContainer = None
 
     def __init__(self):
         self.__mapLoad = Image.open(self.__mapPath)
-        self.__mapWidth = self.__mapLoad.width
-        self.__mapHeight = self.__mapLoad.height
+        self.mapOriginWidth = self.__mapLoad.width
+        self.mapOriginHeight = self.__mapLoad.height
         # 設定地圖的容器，用一個Label物件來裝
         self.__mapContainer = tk.Label(bg='black')
         self.__mapContainer.pack(fill='both', expand=True)  # 全面展開
 
     def Create(self, width=640, height=480):
         # 視窗寬度(高度)小於Map圖片原寬(高)，resize成視窗寬度(高度)，否則維持圖片原寬(高)
-        width = width if width <= self.__mapWidth else self.__mapWidth
-        height = height if height <= self.__mapHeight else self.__mapHeight
+        width = width if width <= self.mapOriginWidth else self.mapOriginWidth
+        height = height if height <= self.mapOriginHeight else self.mapOriginHeight
         # Map異動為指定的大小
         mapImage = self.__mapLoad.resize((width, height))
         # 讀入放Image的容器(Label)
