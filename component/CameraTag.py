714d815601f997e6c2eb2ebc0c4718fae4d0aeb9
SardineBob <yushlai1987@gmail.com>
2020-02-18 15:36:57 +0800

    完成把RTSP播放視窗抽成class

--- component/CameraTag.py
+++ component/CameraTag.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from component.Tag import Tag
+from component.RtspWindow import RtspWindow
 from PIL import Image, ImageTk
 from cv2 import cv2
 import time
@@ -9,21 +10,16 @@ import threading
 class CameraTag(Tag):
 
     __picPath = './resource/IconCamera.png'
-    __rtspUrl = None
-    __rtspTask = None
-    __rtspActive = False
+    __rtspUrl = ''
     __rtspWindow = None
-    __rtspX = 100
-    __rtspY = 100
-    __rtspWidth = 10
-    __rtspHeight = 5
+    __rtspOpen = False
 
     def __init__(self, canvas, configItem):
         # 取出需用到的設定值
         pointid = configItem["number"]
         x = configItem["X"]
         y = configItem["Y"]
-        rtspUrl = configItem["rtspUrl"]
+        self.__rtspUrl = configItem["rtspUrl"]
         # open攝影機標籤的icon image
         picLoad = Image.open(self.__picPath)
         picPhoto = ImageTk.PhotoImage(picLoad)
@@ -33,8 +29,6 @@ class CameraTag(Tag):
         canvas.cameraIcon.append(picPhoto)
         # 傳入父類別，建立攝影機標籤物件
         super().__init__(canvas, pointid, x, y, picPhoto, 'camera')
-        # 初始化RTSP串流來源位址
-        self.__rtspUrl = rtspUrl
         # 綁定Click事件到全部擁有camera這個tags的物件
         canvas.tag_bind(self.tagid, '<Button-1>', self.__CameraClickEvent)
 
@@ -42,45 +36,15 @@ class CameraTag(Tag):
     def __CameraClickEvent(self, event):
         # 判斷是否需建立承載RTSP影像串流的容器物件
         if self.__rtspWindow is None:
-            self.__rtspWindow = tk.Label(
-                bg="black", width=self.__rtspWidth, height=self.__rtspHeight)
-            self.__rtspWindow.place(
-                x=self.__rtspX, y=self.__rtspY, anchor='nw')
+            self.__rtspWindow = RtspWindow({'url': self.__rtspUrl})
         # 點擊第一下開啟影像，第二下關閉影像
-        if self.__rtspActive is False:
-            self.__RtspStart()
+        if self.__rtspOpen is False:
+            self.__rtspOpen = True
+            self.__rtspWindow.Start()
         else:
-            self.__RtspStop()
-
-    # 開始播放RTSP影像串流(建立一個執行序來跑，以免畫面Lock)
-    def __RtspStart(self):
-        if self.__rtspTask is None:
-            self.__rtspTask = threading.Thread(target=self.__RtspPlay)
-            self.__rtspTask.setDaemon(True)
-            self.__rtspActive = True
-            self.__rtspTask.start()
-
-    # 停止播放RTSP串流，並銷毀執行序(需等待python程序GC)
-    def __RtspStop(self):
-        self.__rtspActive = False
-        self.__rtspTask = None  # 停止，執行序清掉(等待python程序GC)，以利下一次觸發
-        self.__rtspWindow.destroy()  # 銷毀
-        self.__rtspWindow = None
-
-    # 執行RTSP影像串流播放的動作
-    def __RtspPlay(self):
-        video = cv2.VideoCapture(self.__rtspUrl)
-        (status, frame) = video.read()
-        while self.__rtspActive and status:
-            (status, frame) = video.read()
-            imgArray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-            image = Image.fromarray(imgArray)
-            photo = ImageTk.PhotoImage(image=image)
-            if hasattr(self.__rtspWindow, 'configure') is True:
-                self.__rtspWindow.configure(
-                    image=photo, width=photo.width(), height=photo.height())
-                self.__rtspWindow.rtspImage = photo
-        video.release()
+            self.__rtspOpen = False
+            self.__rtspWindow.Stop()
+            self.__rtspWindow = None
 
         # 兩個問題
         # 2.視窗放大縮小 重新定位的問題
