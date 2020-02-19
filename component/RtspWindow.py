7a7848f1f4b15e14d8759cc2f31702b37048bcce
SardineBob <yushlai1987@gmail.com>
2020-02-19 12:25:10 +0800

    完成開發RTSP視窗拖移功能

--- component/RtspWindow.py
+++ component/RtspWindow.py
@@ -9,24 +9,29 @@ class RtspWindow():
 
     __window = None
     __url = None
-    __x = 0
-    __y = 0
+    __coordX = 0  # 視窗在frame容器中的座標
+    __coordY = 0
     __width = 10
     __height = 5
     __task = None
     __active = False
     __relocatePara = None  # 視窗resize時，所傳入的參數，記錄下來，移動RTSP Window時，需要將座標根據縮放比例重新計算
+    __preMoveCoordX = 0  # 紀錄RTSP視窗拖移時，前一次的XY座標(計算拖移量用)
+    __preMoveCoordY = 0
 
     def __init__(self, para):
         # 取出需用到的設定值
         self.__url = para["url"]
-        self.__x = para["x"]
-        self.__y = para["y"]
+        self.__coordX = para["x"]
+        self.__coordY = para["y"]
         # 建立承載RTSP影像串流的容器物件
         self.__window = tk.Label(
             bg="black", width=self.__width, height=self.__height)
         # 放置在畫面上
-        self.__window.place(x=self.__x, y=self.__y, anchor='nw')
+        self.__window.place(
+            x=self.__coordX, y=self.__coordY, anchor='nw')
+        # 註冊RTSP視窗點擊事件
+        self.__window.bind('<Button-1>', self.__ClickEvent)
         # 註冊RTSP視窗拖移事件
         self.__window.bind('<B1-Motion>', self.__DragEvent)
 
@@ -65,8 +70,8 @@ class RtspWindow():
         if self.__window is None:
             return
         # 放置目前的XY座標
-        para["oriX"] = self.__x
-        para["oriY"] = self.__y
+        para["oriX"] = self.__coordX
+        para["oriY"] = self.__coordY
         # 取得因為視窗縮放產生的新座標
         result = Relocate(para)
         newX = result["newX"]
@@ -76,12 +81,24 @@ class RtspWindow():
         # 紀錄這次的參數
         self.__relocatePara = para
 
+    # RTSP視窗點擊事件
+    def __ClickEvent(self, event):
+        self.__preMoveCoordX = event.x
+        self.__preMoveCoordY = event.y
+
+    # RTSP視窗拖移事件
     def __DragEvent(self, event):
-        print(event.x, event.y)
-        self.__x = event.x
-        self.__y = 100
+        # 這個移動的座標是window內部的，基準點也就是window左上角為(0,0)
+        moveInWindowX = event.x
+        moveInWindowY = event.y
+        # 這個座標是RTSP視窗在frame容器中的座標位置
+        newX = self.__coordX + (moveInWindowX - self.__preMoveCoordX)
+        newY = self.__coordY + (moveInWindowY - self.__preMoveCoordY)
+        # 更新RTSP視窗目前座標
+        self.__coordX = newX
+        self.__coordY = newY
         # 若曾經縮放過，則這個新座標要根據比例縮放
         if self.__relocatePara is None:
-            self.__window.place(x=self.__x, y=self.__y, anchor='nw')
+            self.__window.place(x=newX, y=newY, anchor='nw')
         else:
             self.Relocate(self.__relocatePara)
